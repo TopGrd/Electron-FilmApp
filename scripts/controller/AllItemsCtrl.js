a7b2bc17850ccc5f24f62b11e900cf6a9dcfa72c
topgrd <topgrd@gmail.com>
2016-04-18 16:27:06 +0800

    [allItems page] overlay. rating

--- scripts/controller/AllItemsCtrl.js
+++ scripts/controller/AllItemsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('app')
-	.controller('AllItemsCtrl', ['DataService', '$scope', '$q', '$interval', function(DataService, $scope, $q, $interval) {
-        var self = this;
+	.controller('AllItemsCtrl', ['DataService', '$scope', '$q', '$interval', '$mdIcon',function(DataService, $scope, $q, $interval, $mdIcon) {
+		var self = this;
 		self.activated = true;
 		self.determinateValue = 30;
 		// Iterate every 100ms, non-stop and increment
@@ -12,7 +12,7 @@ angular.module('app')
 			}
 		}, 100);
 
-        var options = {
+		var options = {
 			url: 'https://api.douban.com/v2/movie/in_theaters?count=50',
 			headers: {
 				'User-agent': 'request'
@@ -24,6 +24,7 @@ angular.module('app')
 				self.activated = false;
 				$scope.allItems = data;
 				console.log($scope.allItems);
+				console.log($mdIcon);
 			})
 			.then(function() {
 				onCompletion();
@@ -36,4 +37,29 @@ angular.module('app')
 				myScroll.refresh();
 			}, 200);
 		}
+
+		$scope.renderRating = function (item) {
+			if (item.render === undefined) {
+				var renderTarget = event.currentTarget.children[0].children[0].children[0];
+				var rate = (item.rating.average)/2;
+				var urls = [
+					'img/icons/star.svg',
+					'img/icons/half-star.svg',
+					'img/icons/star_border.svg'
+				];
+				for (var i = 1; i < rate; i++) {
+					//var target = iconElement.cloneNode(true);
+					//renderTarget.appendChild(target);
+					$mdIcon(urls[0]).then(function(iconEle) {
+						renderTarget.appendChild(iconEle);
+					});
+				}
+
+				item.render = true;
+			}
+			else {
+				return;
+			}
+
+		};
 	}]);
